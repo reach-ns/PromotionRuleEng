28f650ccc51884bf8511a5ca079588d64831c17a
Nandish.Siddaramappa <nandish.siddaramappa@shell.com>
2020-07-02 12:15:21 +0530

    Client project creation

---
+++ PromotionClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PromotionClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
